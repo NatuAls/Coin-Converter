48ca01a9552e6184db8ff46dc37be78153a5c510
NatuAls <nahuelalesso19@gmail.com>
2024-04-24 22:49:39 +0200

    WrongInputManager class finished

--- src/com/aluracursos/coinconverter/models/WrongInputManager.java
+++ src/com/aluracursos/coinconverter/models/WrongInputManager.java
@@ -1,2 +1,16 @@
-package com.aluracursos.coinconverter.models;public class WrongInputManager {
+package com.aluracursos.coinconverter.models;
+
+public class WrongInputManager {
+    public boolean option(String option){
+        return option.matches("^([12345678])$");
+    }
+
+    public boolean amount(String amount){
+        try{
+            Double.parseDouble(amount);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
 }
