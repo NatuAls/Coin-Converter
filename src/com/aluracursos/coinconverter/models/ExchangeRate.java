e666ff59085197613fe254f0796836fa45039c46
NatuAls <nahuelalesso19@gmail.com>
2024-04-24 22:46:33 +0200

    ExchangeRate class finished

--- src/com/aluracursos/coinconverter/models/ExchangeRate.java
+++ src/com/aluracursos/coinconverter/models/ExchangeRate.java
@@ -1,2 +1,4 @@
-package com.aluracursos.coinconverter.models;public record ExchangeRate() {
+package com.aluracursos.coinconverter.models;
+
+public record ExchangeRate(String base_code, String target_code, double conversion_result) {
 }
