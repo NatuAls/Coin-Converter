6b074a0aba0886d1722fd6bf74efea9f823c0f33
NatuAls <nahuelalesso19@gmail.com>
2024-04-29 14:24:50 +0200

    refactor Converter class

--- src/com/aluracursos/coinconverter/models/Converter.java
+++ src/com/aluracursos/coinconverter/models/Converter.java
@@ -3,16 +3,16 @@ package com.aluracursos.coinconverter.models;
 public class Converter {
     private String baseCoin;
     private String targetCoin;
-    private double result;
+    private double conversionResult;
 
     public Converter(ExchangeRate exchangeRate){
         this.baseCoin = exchangeRate.base_code();
         this.targetCoin = exchangeRate.target_code();
-        this.result = exchangeRate.conversion_result();
+        this.conversionResult = exchangeRate.conversion_result();
     }
 
     public String convert(double amount){
-        return "El valor %.2f [%s] corresponde al valor final de ==> %.2f [%S]\n"
-                .formatted(amount, this.baseCoin, this.result, this.targetCoin);
+        return "\nEl valor %.2f [%s] corresponde al valor final de ==> %.2f [%S]\n"
+                .formatted(amount, this.baseCoin, this.conversionResult, this.targetCoin);
     }
 }
